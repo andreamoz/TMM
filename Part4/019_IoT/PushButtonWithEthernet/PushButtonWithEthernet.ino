1c72de560a9fd19fc180a37cda155217e41771aa
amaietta <amaietta@gpawidegroup.com>
2013-06-26 09:33:42 +0200

    Initial upload, same as base version

---
+++ Part4/019_IoT/PushButtonWithEthernet/PushButtonWithEthernet.ino
@@ -0,0 +1,15 @@
+int button = 2;
+int led = 3;
+
+void setup(){
+  pinMode(button, INPUT);
+  pinMode(led, OUTPUT);
+}
+
+void loop() { 
+  if (digitalRead(button) == HIGH) {
+    digitalWrite(led, HIGH);        
+    // ask php page to tweet
+    digitalWrite(led, LOW);
+  }
+}
